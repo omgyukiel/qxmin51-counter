dc533ab57ab5e3044e2a78be5efbfa2e323d08aa
Kenny <kennylevu1@gmail.com>
2024-01-29 14:49:09 -0800

    implemented counter with rollover and buttons

--- Display/Display.c
+++ Display/Display.c
@@ -5,6 +5,11 @@ __code unsigned char table[] = { 0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82,
 	0xf8, 0x80, 0x90
 };
 
+#define	K4	P3_2
+#define	K3	P3_3
+#define	K2	P3_4
+#define	K1	P3_5
+
 void delay(unsigned int i)
 {
 	unsigned char j;
@@ -16,13 +21,13 @@ void delay(unsigned int i)
 
 void display(int v)
 {   
-    if (v == 10000) {
-        v = 0;
-    }
+    // if (v > 9999) {
+    //     v = 0;
+    // }
 
-    if (v == -1) {
-        v = 9999;
-    }
+    // if (v < 0) {
+    //     v = 9999;
+    // }
 
 	P0 = table[(v/1000)];
 	P2_0 = 0;
@@ -51,6 +56,27 @@ void main(void)
     int v = 0;
 
 	while (1) {
-        display(4321);
+        // 0 = button press
+        if (K3 == 0) {
+            delay(5);
+            if (K3 == 0) {
+                // while(!K3);
+                v++;
+                if (v > 9999) {
+                    v = 0;
+                }
+            }
+        }
+        if (K4 == 0) {
+            delay(5);
+            if (K4 == 0) {
+                // while(!K4);
+                v--;
+                if (v < 0) {
+                    v = 9999;
+                }
+            }
+        }
+        display(v);
 	}
 }
