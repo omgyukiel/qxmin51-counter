89aecaab4e9aa35e319a877a8ec1974b41097651
Kenny <kennylevu1@gmail.com>
2024-02-01 13:59:22 -0800

    fixed bug where types werent defined, causing garbled data

--- LCD/lcd.c
+++ LCD/lcd.c
@@ -1,5 +1,4 @@
 #include <8051.h>
-
 #define SDA P0_0
 #define SCL P0_1
 #define INT P0_2
@@ -15,7 +14,7 @@ void delay(unsigned int t)
 {
 	while (--t); // roughly i * 8 + 10 µs
 }	
-
+// sends 8 bit command through serial
 void cmdout(unsigned char cmd) 
 {
     for (unsigned char i = 0; i < 8; i++) {
@@ -32,28 +31,6 @@ void cmdout(unsigned char cmd)
     }
     SCL = 0;
 }
-
-void pcf_command
-void lcd_set(unsigned char cmd) 
-{
-    for (unsigned char i = 0; i < 3; i++) {
-        if (cmd & LEDMSK) {
-            SDA = 1;
-        }
-        else {
-            SDA = 0;
-        }
-        delay(10);
-    }
-
-        SCL = 1;
-        cmd = cmd << 1;  // clock data into sda
-        SCL = 0;
-    
-    // SDA = 0;
-    SCL = 0;
-}
-
 void cmdin(void) 
 {
     for (int i = 7; i >= 0; i--) {
@@ -101,7 +78,7 @@ void delay_led(unsigned int i)
 void wack(void) {
      // ACK from slave
         SCL = 1;
-        delay(100);
+        delay(50);
         if (SDA) {  // NACK
             P1 = 0x00;
             delay(100);
@@ -132,105 +109,138 @@ void rack(unsigned char valid) {
         SCL = 0;
 }
 
-void lcd_write(unsigned char cmd) {
-    cmdout(0x7e);
+// write bytes amount of commands to i2c
+void pcf_write_command(unsigned char *commands, int bytes) 
+{
+    start(); // start condition for i2c device;
+    cmdout(0x7e); // set i2c at address 3f in write mode
     delay(10);
-    wack();
-    cmdout(cmd);
-    wack();
+    wack(); // receive ack/nack from slave
+    for (int i = 0; i < bytes; i++) { // SEND all commands in array
+        cmdout(commands[i]);
+        delay(10);
+        wack(); // receive ack/nack from slave
+    }
+    stop(); // stop condition for i2c device;
 }
 
-void lcd_read(void) {
-    cmdout(0x7f);
-    delay(10);
-    wack();
-    cmdin();
-    rack(ACK);
+/* https://www.youtube.com/watch?v=WAGLQOIHEio&ab_channel=ControllersTech
+   upper nibble data bus lines d7 d6 d5 d4, for p7 p6 p5 p4 respectively     (bits 7 6 5 4)
+   lower nibbles are backlight, enable, r/w, register select for p3 p2 p1 p0 respectively (bits 3 2 1 0)
+   register select is high when writing to the lcd, low when sending a command
+   commands must be clocked or "strobed" by setting the enable line off and off,so each command is sent twice to the lcd
+   r/w is only set high when reading from lcd */
+
+
+//sends command to lcd through i2c 
+// note 1 byte = P7 P6 P5 P4 P3 P2 P1 P0  == D7 D6 D5 D4 LED E R/W RS
+void lcd_cmd(unsigned char cmd) 
+{
+    unsigned char upper, lower; // upper, lower nibbles
+    unsigned char data_t[4];
+    int bytes = 4;
+    upper = (cmd&0xf0); // upper nibble
+    lower = ( (cmd<<4) & 0xf0); // lower nibble
+    // strobe commands by togglign enable line, rs = 0 to signal a command
+    data_t[0] = upper | 0x0c; // en = 1 , rs = 0
+    data_t[1] = upper | 0x08; // en = 0 , rs = 0
+    data_t[2] = lower | 0x0c; 
+    data_t[3] = lower | 0x08;
+    pcf_write_command(data_t, bytes); 
 }
 
-void LCD_initialize(void) {
-
+// writes data to lcd through i2c, same as command but with register select set to high
+void lcd_write(unsigned char cmd) 
+{
+    unsigned char upper, lower; // upper, lower nibbles
+    unsigned char data_t[4];
+    int bytes = 4;;
+    upper = (cmd&0xf0); // upper nibble
+    lower = ( (cmd<<4) & 0xf0); // lower nibble
+    // strobe commands by togglign enable line, rs = 0 to signal a command
+    data_t[0] = upper | 0x0d; // en = 1 , rs = 1
+    data_t[1] = upper | 0x09; // en = 0 , rs = 1
+    data_t[2] = lower | 0x0d; 
+    data_t[3] = lower | 0x09; 
+    pcf_write_command(data_t, bytes); 
 }
 
 
-// https://www.youtube.com/watch?v=WAGLQOIHEio&ab_channel=ControllersTech
-// upper nibble data bus lines d7 d6 d5 d4, for p7 p6 p5 p4 respectively     (bits 7 6 5 4)
-// lower nibbles are backlight, enable, r/w, register select for p3 p2 p1 p0 respectively (bits 3 2 1 0)
-// register select is high when writing to the lcd, low when sending a command
-// r/w is only set high when reading from lcd
+void lcd_clear (void)
+{
+	lcd_cmd (0x01);
+	delay(300);
+}
+
+void lcd_put_cur(int row, int col)
+{
+    switch (row)
+    {
+        case 0:
+            col |= 0x80;
+            break;
+        case 1:
+            col |= 0xC0;
+            break;
+    }
+
+    lcd_cmd (col);
+}
+
+void lcd_send_string (char *str)
+{
+	while (*str) lcd_write (*str++);
+}
+
+void lcd_init(void) {
+    // initializes 4 bit mode
+    // init source is in espi2clcd_2.jpeg within resources
+    // https://controllerstech.com/i2c-in-esp32-esp-idf-lcd-1602/#info_box
+    // note 1 byte = P7 P6 P5 P4 P3 P2 P1 P0  == D7 D6 D5 D4 LED E R/W RS
+
+    delay(2800); // delay >40ms
+    lcd_cmd(0x30);
+    delay(300); // >4.1ms
+    lcd_cmd(0x30);
+	delay(10);  // >100us
+    lcd_cmd(0x30);
+	delay(600);
+    lcd_cmd(0x20); // 4 bit mode
+	delay(600);
+
+    // dislay initialisation
+	lcd_cmd (0x28); // Function set --> DL=0 (4 bit mode), N = 1 (2 line display) F = 0 (5x8 characters) //28 is old
+	delay(55);
+	lcd_cmd(0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
+	delay(55);
+	lcd_cmd(0x01);  // clear display
+	delay(55);
+	delay(55);
+	lcd_cmd(0x06); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
+	delay(55);
+	lcd_cmd(0x0C); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
+	delay(112);
+}
+
 void main(void)  
 {  
-    delay_led(1000);
-    
+    delay(1000);
+    lcd_init();
+    delay(1000);
+    // lcd_write("A");
+    lcd_clear();
+    delay(55);
+        lcd_put_cur(0, 3);
+        delay(55);
+    lcd_cmd(0x02);
+    delay(55);
+    delay(55);
+    lcd_send_string("Hello dudes");
 
     while(1) {
-        // LCD addresses: 7e write, 7f read
-        start();
-        lcd_write(0x0c);
-        stop();
-        delay_led(100);
 
-        // start();
-        // lcd_read();
-        // stop();
-        break;
-        
-        cmdout(0x38);  // dl = 8, lines = 2
-        wack();
 
-        cmdout(0x00);
-        wack();
-        cmdout(0x0c); // display on
-        wack();
+    }
 
-        cmdout(0x00);
-        wack();
-        cmdout(0x06); // cursor direction increments
-        wack();
-
-        cmdout(0x00);
-        wack();
-        cmdout(0x01); // clear displayand reset add to 0
-        wack();
-
-        cmdout(0x00);
-        wack();
-        cmdout(0x02); // reset cursor
-        wack();
-
-        stop();
-        // delay_led(100000);
-
-}
-
-        // unsigned char cmd = 0x0F;
-    //     unsigned char cmd = 0b00001100; // lower bytes three bits are r rw e 
-    //     for (unsigned char i = 0; i < 8; i++) {
-    //         if (cmd & 0x80) {
-    //             SDA = 1;
-    //         }
-    //         else {
-    //             SDA = 0;
-    //         }
-    //         delay(10);
-    //         SCL = 1;
-    //         cmd = cmd << 1;  // clock data into sda
-    //         SCL = 0;
-    //     }
-    //     SCL = 0;
-
-    //      // ACK from slave
-    //     SCL = 1;
-    //     delay(1);
-    //     if (SDA) {  // NACK
-    //         P1 = 0x00;
-    //         delay(100);
-    //         P1 = 0xFF;
-    //         // stop();
-    //     } else { // SDA low is ACK
-    //         P1 = 0xFF;
-    //         delay(100);
-    //     }
-    //     SCL = 0;
 }
 
