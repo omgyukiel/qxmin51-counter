10dea1796f4516a561046e09b37dbe41c2d825b5
Kenny <kennylevu1@gmail.com>
2024-02-02 10:07:21 -0800

    updated read me and cleaned up main file

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-• Using a make file: make && stcgal -p COM8 -P stc89 -a LedBlank.ihx
-• Using sdcc: sdcc LedBlink.c && stcgal -p COM8 -P stc89 LedBlink.ihx
-
+// https://wiki.laptop.org/go/SDCC#Compiling_a_program
 Steps:
-Compile C file using sdcc
-ex: sdcc LedBlink.c
+Compile C files using sdcc
 
-ISP Flash the ihx file using stcgal
-ex:
-stcgal -p COM8 -P stc89 LedBlink.ihx
+The ﬁrst two ﬁles will need to be compiled separately with the commands:
+sdcc -c i2c.c
+sdcc -c lcd.c
+Then compile the source ﬁle containing the main() function and link the ﬁles together with the following command:
+sdcc main.c i2c.rel lcd.rel
+
+Alternatively, main.c can be separately compiled as well:
+sdcc -c main.c
+sdcc main.rel i2c.rel lcd.rel
+
+
+ISP Flash the ihx file using stcgal:
+stcgal -p COM8 -P stc89 main.ihx
+
 -p sets serial port of MCU, -P sets the protocol f, LedBlink.ihx is the path to the code image
-
--- src/main.c
+++ src/main.c
@@ -18,41 +18,9 @@
 #include "periphs.h"
 #include "constants.h"
 
-// #include <stdint.h>
-// // K1 - K4 are keys S1-S4 respectively
-// #define	K4	P3_2
-// #define	K3	P3_3
-// #define	K2	P3_4
-// #define	K1	P3_5
-
-// #define SEG 0
-// #define COUNT 1
-// #define CYCLON 2
-// #define DUAL 3
-// #define SDA P0_0
-// #define SCL P0_1
-// #define INT P0_2
-// #define DBG P0_3
-// #define MSK 0x80
-// #define ACK 0
-// #define NACK 1
-// 0 bit means that segment is on
-// dictionary for digits 0-9 on the 7-segment display
 __code unsigned char table[] = { 0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82,
 	0xf8, 0x80, 0x90
 };
-
-
-
-
-// // program a delay between operations
-// void delay_led(unsigned int i)
-// {
-// 	unsigned char j;
-
-// 	for (; i > 0; i--)
-// 		for (j = 255; j > 0; j--);
-// }
 
 
 // displays decimal 0-9999
@@ -82,116 +50,6 @@ void display_seg(int v)
 	P2_3 = 1;
 
 }
-
-
-// // source: https://github.com/retiredfeline/QX-mini51-SDCC
-// // delay for 18  µs
-// void delay(unsigned int t)
-// {
-// 	while (--t); // roughly i * 8 + 10 µs
-// }	
-// // start condition for i2c op
-// void start(void) {
-//     SDA = 1;
-//     delay(1);
-//     SCL = 1;
-//     SDA = 0;
-//     delay(1);
-//     SCL = 0;
-// }
-
-// // stop condition for i2c op
-// void stop(void) {
-//     SDA = 0;
-//     delay(1);
-//     SCL = 1;
-//     SDA = 1;
-// }
-
-// // sends byte instruction to i2c
-// void cmdout(unsigned char cmd) 
-// {
-//     for (unsigned char i = 0; i < 8; i++) {
-//         if (cmd & MSK) {
-//             SDA = 1;
-//         }
-//         else {
-//             SDA = 0;
-//         }
-//         delay(10);
-//         SCL = 1;
-//         cmd = cmd << 1;  // clock data into sda
-//         SCL = 0;
-//     }
-// }
-
-
-// // ack from slave
-// void wack(void) {
-//      // ACK from slave
-//         SCL = 1;
-//         delay(1);
-//         if (SDA) {  // NACK
-//         // do something
-//         } else { // SDA low is ACK
-//         // do something
-//         }
-//         SCL = 0;
-// }
-// // write bytes amount of commands to i2c
-// void pcf_write_command(uint8_t *commands, int bytes) {
-//     start(); // start condition for i2c device;
-//     cmdout(0x7e); // set i2c at address 3f in write mode
-//     delay(1);
-//     wack(); // receive ack/nack from slave
-//     for (int i = 0; i < bytes; i++) { // SEND all commands in array
-//         cmdout(commands[i]);
-//         delay(1);
-//         wack(); // receive ack/nack from slave
-//     }
-//     stop(); // stop condition for i2c device;
-// }
-// /* https://www.youtube.com/watch?v=WAGLQOIHEio&ab_channel=ControllersTech
-//    upper nibble data bus lines d7 d6 d5 d4, for p7 p6 p5 p4 respectively     (bits 7 6 5 4)
-//    lower nibbles are backlight, enable, r/w, register select for p3 p2 p1 p0 respectively (bits 3 2 1 0)
-//    register select is high when writing to the lcd, low when sending a command
-//    commands must be clocked or "strobed" by setting the enable line off and off,so each command is sent twice to the lcd
-//    r/w is only set high when reading from lcd 
-//    note 1 byte = P7 P6 P5 P4 P3 P2 P1 P0  == D7 D6 D5 D4 LED E R/W RS
-//    */
-// // sends command to lcd through i2c 
-// void lcd_cmd(char cmd) 
-// {
-//     char upper, lower; // upper, lower nibbles
-//     uint8_t data_t[4];
-//     int bytes = 4;
-//     upper = (cmd&0xf0); // upper nibble
-//     lower = ( (cmd<<4) & 0xf0); // lower nibble
-//     // strobe commands by togglign enable line, rs = 0 to signal a command
-//     data_t[0] = upper | 0x0c; // en = 1 , rs = 0
-//     data_t[1] = upper | 0x08; // en = 0 , rs = 0
-//     data_t[2] = lower | 0x0c; 
-//     data_t[3] = lower | 0x08;
-//     pcf_write_command(data_t, bytes); 
-// }
-// void lcd_write(char cmd) 
-// {
-//     char upper, lower; // upper, lower nibbles
-//     uint8_t data_t[4];
-//     int bytes = 4;;
-//     upper = (cmd&0xf0); // upper nibble
-//     lower = ( (cmd<<4) & 0xf0); // lower nibble
-//     // strobe commands by togglign enable line, rs = 0 to signal a command
-//     data_t[0] = upper | 0x0d; // en = 1 , rs = 1
-//     data_t[1] = upper | 0x09; // en = 0 , rs = 1
-//     data_t[2] = lower | 0x0d; 
-//     data_t[3] = lower | 0x09; 
-//     pcf_write_command(data_t, bytes); 
-// }
-// void lcd_send_string (char *str)
-// {
-// 	while (*str) lcd_write (*str++);
-// }
 
 void setup(void) {
     // startup sound
